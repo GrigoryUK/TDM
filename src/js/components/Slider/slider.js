62ce371c0994e0d67065411bbeac410ce8fb0510
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2022-12-06 11:05:30 +0500

    06.12. start

--- src/js/components/Slider/slider.js
+++ src/js/components/Slider/slider.js
@@ -2,7 +2,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
 function sliderHorizontal() {
-  const swiper = new Swiper('.slider--horizontal', {
+  const swiperH = new Swiper('.slider--horizontal', {
     slidesPerView: 'auto',
     spaceBetween: 20,
     loop: true,
@@ -10,6 +10,17 @@ function sliderHorizontal() {
     navigation: {
       nextEl: '.sliderH__next',
       prevEl: '.sliderH__prev',
+    },
+  });
+
+  const swiperV = new Swiper('.slider--vertical', {
+    slidesPerView: 'auto',
+
+    loop: true,
+
+    navigation: {
+      nextEl: '.sliderV__next',
+      prevEl: '.sliderV__prev',
     },
   });
 }
