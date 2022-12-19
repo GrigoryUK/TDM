98d1c2f953fd161045f9a52f598a7095bafbfd43
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2022-12-19 13:22:14 +0500

    fix

--- src/js/components/utils/accordion.js
+++ src/js/components/utils/accordion.js
@@ -1,29 +1,37 @@
-
+import { isMobile, isTablet, isDesktop } from "../../functions/check-viewport";
 
 function accordion() {
 
   const accordions = document.querySelectorAll('.accordion');
 
-	accordions.forEach(el => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const control = self.querySelector('button');
-			const content = self.querySelector('ul');
+  if (isMobile()) {
+    if (accordions) {
+      accordions.forEach(el => {
+        el.addEventListener('click', (e) => {
+          const self = e.currentTarget;
+          const control = self.querySelector('button');
+          const content = self.querySelector('ul');
 
-			self.classList.toggle('open');
+          self.classList.toggle('open');
 
-			// если открыт аккордеон
-			if (self.classList.contains('open')) {
-				control.setAttribute('aria-expanded', true);
-				content.setAttribute('aria-hidden', false);
-				content.style.maxHeight = content.scrollHeight + 'px';
-			} else {
-				control.setAttribute('aria-expanded', false);
-				content.setAttribute('aria-hidden', true);
-				content.style.maxHeight = null;
-			}
-		});
-	});
+          // если открыт аккордеон
+          if (self.classList.contains('open')) {
+            control.setAttribute('aria-expanded', true);
+            content.setAttribute('aria-hidden', false);
+            content.style.maxHeight = content.scrollHeight + 'px';
+          } else {
+            control.setAttribute('aria-expanded', false);
+            content.setAttribute('aria-hidden', true);
+            content.style.maxHeight = null;
+          }
+        });
+      });
+    }
+  }
+
+
+
+
 }
 
 export default accordion;
